efb6687c64ec325ebe4d10b7ea1a4eb5cb249ab4
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-07-02 17:57:58 -0400

    Removed commented out code

--- controllers/imagesController.js
+++ controllers/imagesController.js
@@ -1,7 +1,4 @@
 const knex = require("knex")(require("../knexfile").development);
-const axios = require("axios");
-const fs = require("fs");
-const keywordsFilePath = "./data/keywords.json";
 
 exports.addImages = (req, res) => {
   knex("images")
@@ -13,40 +10,3 @@ exports.addImages = (req, res) => {
       res.status(400).send(`Error creating Images: ${err}`);
     });
 };
-
-// const getKeywordData = () => {
-//   return JSON.parse(fs.readFileSync(keywordsFilePath));
-// };
-
-// exports.generateImages = (req, res) => {
-//  //get keyword from front end
-//  let requestedKeyword = req.params.keyword;
-//  console.log(requestedKeyword);
-//  //go to colors_keywords database
-//  let lookupFile = getKeywordData();
-//  //match keyword
-//  const foundK = lookupFile.find((listItem) => {
-//    if (listItem.keyword === requestedKeyword) {
-//      console.log(listItem.colorHex);
-//      return listItem.colorHex;
-//    }
-//  });
-//  console.log(foundBaseColor.colorHex);
-
-//  axios
-//    .get(
-//      `https://www.thecolorapi.com/scheme?hex=${foundBaseColor.colorHex}&count=5`
-//    )
-//    .then((response) => {
-//      let colorPalette = response.data.colors.map((color) => {
-//        return {
-//          color: color.hex,
-//        };
-//      });
-
-//      res.json(colorPalette);
-//    })
-//    .catch((err) => {
-//      res.status(400).send(`Error retrieving color palette: ${err}`);
-//    });
-// };
