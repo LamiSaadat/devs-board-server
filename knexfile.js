595fa6fb6624ef6486f2b44374f951d5e45a2476
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-07-05 21:04:03 -0400

    Update dotenv file with databse info

--- knexfile.js
+++ knexfile.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
+
 module.exports = {
   development: {
     client: "mysql",
     connection: {
-      host: "127.0.0.1",
-      user: "root",
-      password: "rootroot",
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PSWD,
       database: "devsboard_database",
       charset: "utf8",
     },
