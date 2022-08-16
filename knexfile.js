3062afc52c322f5eff5b054f3dd1fd3dd7159d88
Lamisa Saadat <ls.lamisa12@gmail.com>
2022-08-16 19:05:49 -0400

    Updated knex config file for production

--- knexfile.js
+++ knexfile.js
@@ -1,30 +1,30 @@
 require("dotenv").config();
 
-// module.exports = {
-//   development: {
-//     client: "mysql",
-//     connection: process.env.JAWSDB_URL,
-//   },
-// };
-
-const connections = {
+module.exports = {
   development: {
-    client: process.env.CLIENT,
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PSWD,
-      database: "devsboard_database",
-      charset: "utf8",
-    },
-  },
-  production: {
-    client: process.env.CLIENT,
+    client: "mysql",
     connection: process.env.JAWSDB_URL,
   },
 };
 
-module.exports =
-  process.env.NODE_ENV === "production"
-    ? connections.production
-    : connections.development;
+// const connections = {
+//   development: {
+//     client: process.env.CLIENT,
+//     connection: {
+//       host: process.env.DB_HOST,
+//       user: process.env.DB_USER,
+//       password: process.env.DB_PSWD,
+//       database: "devsboard_database",
+//       charset: "utf8",
+//     },
+//   },
+//   production: {
+//     client: process.env.CLIENT,
+//     connection: process.env.JAWSDB_URL,
+//   },
+// };
+
+// module.exports =
+//   process.env.NODE_ENV === "production"
+//     ? connections.production
+//     : connections.development;
